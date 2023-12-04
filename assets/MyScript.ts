c94741188129723cd7226d41653d17e2c70ab0ce
Daniel Winther <daniel.kirgiopoulos@gmail.com>
2023-12-04 22:20:10 +0100

    chore: initial public release

---
+++ assets/MyScript.ts
@@ -0,0 +1,81 @@
+import {
+    _decorator,
+    Component,
+} from "cc";
+
+const { ccclass } = _decorator;
+import { Appylar } from "./Appylar";
+
+@ccclass('MyScript')
+export class MyScript extends Component implements AppylarInitializationListener, AppylarBannerListener, AppylarInterstitialListener {
+    configKeys: any = {
+        appKeyIos: "<appKeyIos>",
+        appKeyAndroid: "<appKeyAndroid>",
+        appIdIos: "<appIdIos>",
+        appIdAndroid: "<appIdAndroid>"
+    };
+    public Appylar: Appylar = null;
+
+    onNoInterstitial(): void {
+        const response = "onNoInterstitial"
+        console.log("Response", response);
+    }
+    onInterstitialShown(): void {
+        const response = "onInterstitialShown"
+        console.log("Response", response);
+    }
+    onInterstitialClosed(): void {
+        const response = "onInterstitialClosed"
+        console.log("Response", response);
+    }
+    onNoBanner(): void {
+        const response = "onNoBanner"
+        console.log("Response", response);
+    }
+    onBannerShown(height: number): void {
+        const response = "onBannerShown" + height
+        console.log("Response", response);
+    }
+
+    onInitialized(): void {
+        const response = "onInitialized";
+        console.log("Response", response);
+    }
+    
+    protected onLoad(): void {
+        this.Appylar = new Appylar(this.node);
+    }
+    async initialization() {
+        const adTypes = ['banner', 'interstitial']; // Add both or one of your choice here.
+        try {
+            await this.Appylar.init(this.configKeys, adTypes, true, this);
+        } catch (e) {
+            console.log(e, 'error')
+        }
+    }
+    async showTopAd() {
+        this.Appylar.showBannerAd("top", this, 'placement Text');
+    }
+    async showBottomAd() {
+        this.Appylar.showBannerAd("bottom", this, 'placement Text');
+    }
+    async hideBannerAd() {
+        this.Appylar.hideBanner();
+    }
+    async showInterstitialAd() {
+        this.Appylar.showInterstitialAd(this, 'placement Text');
+    }
+    async setParameters() {
+        const parameters: Map<string, string[]> = new Map<string, string[]>()
+        parameters['banner_height'] = ["90"]
+        this.Appylar.setParameters(parameters);
+    }
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+
+    }
+}
+
